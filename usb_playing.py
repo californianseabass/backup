62c8131a59426b8b3012dc5c8473de491c6ba3a1
californianSeaBass <releaseTheSeaBass@gmail.com>
2013-04-21 19:04:37 -0700

    trying to read usb after unmount .....

--- usb_playing.py
+++ usb_playing.py
@@ -1,7 +1,8 @@
 # /import usb.core
 # import usb.util
 
-import dircache, commands
+import dircache, commands, re
+from os import listdir
 
 base = '/Volumes/'
 volumes = dircache.listdir(base)
@@ -14,10 +15,26 @@ for vol in volumes:
 		if result[0] != 0:
 			print "Something went wrong unmounting %s" % (path)
 			sys.exit(1);
-		result = commands.getstatusoutput('diskutil eject %s' % (path))
-		if result[0] != 0:
-			print "Something went wrong ejecting %s" % (path)
-			sys.exit(1); 
-		print "Successfully umounted and ejected %s" % (path)
+		# result = commands.getstatusoutput('diskutil eject %s' % (path))
+		# if result[0] != 0:
+		# 	print "Something went wrong ejecting %s" % (path)
+		# 	sys.exit(1); 
+		print "Successfully umounted  %s" % (path)
+
 	else:
 		print "don't touch Simba please!!!"
+
+base = '/dev/'
+volumes = dircache.listdir(base)
+
+
+for vol in volumes:
+	if (re.match("rdisk*", vol)):
+		path = base + vol
+		if (re.match("rdisk1*", vol) is not 0):
+			print path
+			open(path, 'r')
+
+
+
+
