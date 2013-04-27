d6204adb67efb83e8952c0a544981403d88dcf76
californianSeaBass <releaseTheSeaBass@gmail.com>
2013-04-26 22:02:51 -0700

    new xcode project, reads basic info about usb devices in c

--- usb_playing.py
+++ usb_playing.py
@@ -1,63 +1,49 @@
-# /import usb.core
-# import usb.util
+# # /import usb.core
+# # import usb.util
 
-import dircache, io, os, serial, sys
-from datetime import datetime	
-from os import listdir
-from os import path
-from os import makedirs
+# import dircache, io, os, serial, sys
+# from datetime import datetime	
+# from os import listdir
+# from os import path
+# from os import makedirs
 
-startTime = datetime.now()
-base = '/Volumes/'
-volumes = dircache.listdir(base)
+# startTime = datetime.now()
+# base = '/Volumes/'
+# volumes = dircache.listdir(base)
 
-for vol in volumes:
-	if (vol != 'Simba'):
-		curr_path = base + vol + '/'
-		dest_dir = "/Users/californianseabass/Desktop/" + vol
-		if not os.path.isdir(dest_dir):
-			os.mkdir(dest_dir)
-			print "created directory %s" % dest_dir
-		# print curr_path
-		# result = commands.getstatusoutput('cp -r %s /Users/californianseabass/Desktop/' % (path))
-		files = listdir(curr_path)
-		for file_name in files:
-			srcfile = curr_path + file_name
-			if (path.isfile(srcfile)):
-				fd_read = io.open(srcfile, 'rb')
-				if (fd_read.name == "/Volumes/TEST/._.Trashes"):
-					print "dont' do this"
-				else:
-					dest = dest_dir + '/' +file_name
-					dest_buf = open(dest, 'w+')
-					rbuf = io.BufferedReader(fd_read, 4096)
-					data = rbuf.read()
-					while (data):
-						dest_buf.write(data)
-						data = rbuf.read()
-					fd_read.close()
-	else:
-		print "don't touch Simba please"
+# for vol in volumes:
+# 	if (vol != 'Simba'):
+# 		curr_path = base + vol + '/'
+# 		dest_dir = "/Users/californianseabass/Desktop/" + vol
+# 		if not os.path.isdir(dest_dir):
+# 			os.mkdir(dest_dir)
+# 			print "created directory %s" % dest_dir
+# 		# print curr_path
+# 		# result = commands.getstatusoutput('cp -r %s /Users/californianseabass/Desktop/' % (path))
+# 		files = listdir(curr_path)
+# 		for file_name in files:
+# 			srcfile = curr_path + file_name
+# 			if (path.isfile(srcfile)):
+# 				fd_read = io.open(srcfile, 'rb')
+# 				if (fd_read.name == "/Volumes/TEST/._.Trashes"):
+# 					print "dont' do this"
+# 				else:
+# 					dest = dest_dir + '/' +file_name
+# 					dest_buf = open(dest, 'w+')
+# 					rbuf = io.BufferedReader(fd_read, 4096)
+# 					data = rbuf.read()
+# 					while (data):
+# 						dest_buf.write(data)
+# 						data = rbuf.read()
+# 					fd_read.close()
+# 	else:
+# 		print "don't touch Simba please"
 
-print(datetime.now() - startTime)
+# print(datetime.now() - startTime)
 
 
 
-import re
-import subprocess
-device_re = re.compile("Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
-df = subprocess.check_output("system_profiler SPUSBDataType", shell=True)
-devices = []
-for line in df.split('\n'):
-	print line
-    if re.match("USB High-Speed Bus", line):
-    	print line
-        info = device_re.match(i)
-        if info:
-            dinfo = info.groupdict()
-            dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
-            devices.append(dinfo)
-print devices
+
 
 
 # base = '/dev/'
@@ -89,50 +75,50 @@ print devices
 #         dirnames.remove('.git')
 
 
-# import usb
-# import sys
+import usb
+import sys
 
-# dev = usb.core.find(idVendor=0x0bda)
+dev = usb.core.find(idVendor=0x0bda)
 
-# # was it found?
-# if dev is None:
-#     raise ValueError('Device not found')
+# was it found?
+if dev is None:
+    raise ValueError('Device not found')
 
-# if dev.is_kernel_driver_active(0) is True:
-# 	dev.detach_kernel_driver(0)
+if dev.is_kernel_driver_active(0) is True:
+	dev.detach_kernel_driver(0)
 
-# # set the active configuration. With no arguments, the first
-# # configuration will be the active one
-# dev.set_configuration()
+# set the active configuration. With no arguments, the first
+# configuration will be the active one
+dev.set_configuration()
 
-# # get an endpoint instance
-# cfg = dev.get_active_configuration()
-# interface_number = cfg[(0,0)].bInterfaceNumber
-# alternate_setting = usb.control.get_interface(dev, interface_number)
-# intf = usb.util.find_descriptor(cfg, bInterfaceNumber = interface_number,bAlternateSetting = alternate_setting)
+# get an endpoint instance
+cfg = dev.get_active_configuration()
+interface_number = cfg[(0,0)].bInterfaceNumber
+alternate_setting = usb.control.get_interface(dev, interface_number)
+intf = usb.util.find_descriptor(cfg, bInterfaceNumber = interface_number,bAlternateSetting = alternate_setting)
 
-# ep = usb.util.find_descriptor(intf,
-#     # match the first OUT endpoint
-#     custom_match = \
-#     lambda e: \
-#         usb.util.endpoint_direction(e.bEndpointAddress) == \
-#         usb.util.ENDPOINT_OUT
-# )
+ep = usb.util.find_descriptor(intf,
+    # match the first OUT endpoint
+    custom_match = \
+    lambda e: \
+        usb.util.endpoint_direction(e.bEndpointAddress) == \
+        usb.util.ENDPOINT_OUT
+)
 
-# assert ep is not None
+assert ep is not None
 
-# for cfg in dev:
-#     sys.stdout.write(str(cfg.bConfigurationValue) + '\n')
-#     for intf in cfg:
-#         sys.stdout.write('\t' + \
-#                          str(intf.bInterfaceNumber) + \
-#                          ',' + \
-#                          str(intf.bAlternateSetting) + \
-#                          '\n')
-#         for ep in intf:
-#             sys.stdout.write('\t\t' + \
-#                              str(ep.bEndpointAddress) + \
-#                              '\n')
+for cfg in dev:
+    sys.stdout.write(str(cfg.bConfigurationValue) + '\n')
+    for intf in cfg:
+        sys.stdout.write('\t' + \
+                         str(intf.bInterfaceNumber) + \
+                         ',' + \
+                         str(intf.bAlternateSetting) + \
+                         '\n')
+        for ep in intf:
+            sys.stdout.write('\t\t' + \
+                             str(ep.bEndpointAddress) + \
+                             '\n')
 
 
 # cfg.set()
